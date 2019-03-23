9e7f9ca9dd6d6551f60c4375324c9fec3a1f9781
HeshamSayed <hesham.sayed636@gmail.com>
2019-03-23 19:29:16 +0200

    fixing registeration path in home

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users, controllers: {
       sessions: 'users/sessions'}
+
   root to: 'home#index'
   resources :orders do
     resources :details
