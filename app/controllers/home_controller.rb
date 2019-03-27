dd834d284b0a285427c2c2c20f1342b56c7d3d53
“fatemagit config --global user.name “fatema <fatemamo7amed@gmail.com>
2019-03-27 14:51:39 +0200

    knowing friends activity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @lastOrder = Order.where(user_id: current_user.id).order("id asc").last(5)
+    @lastOrder = Order.where(user_id: current_user.id).order("id Desc").last(5)
+   
+    @friendActivity = Order.where(
+      user_id: Friend.select("friend_id")
+      .where(user_id: current_user.id)
+  ).order("id Desc").last(4)
+
   end
 end
