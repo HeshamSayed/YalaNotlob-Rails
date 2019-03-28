af165a0fac1b3a464e205fb3b4a39c1bda0da289
aml <aml.fci@gmail.com>
2019-03-28 21:54:31 +0200

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,16 +20,16 @@ class User < ApplicationRecord
       end
     end
   end
-
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.image = auth.info.image
-    user.username = auth.info.username
-    user.oauth_token = auth.credentials.token
-    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-    user.password = Devise.friendly_token[0,20]
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # user.skip_confirmation!
+    end
+  end
 
-  end
-  end
+
 end  
