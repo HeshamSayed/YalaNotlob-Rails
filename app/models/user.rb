bc16dccd7852fa90130e25e953b28bcfdaba5acd
amlmoustafa <45965529+amlmoustafa@users.noreply.github.com>
2019-03-29 11:08:15 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
-
   has_many :groups
   has_many :users, class_name: "Friend", foreign_key: "user_id"
   has_many :order
   has_many :friend
 
-  
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.username   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # user.skip_confirmation!
-    end
+    user.email = auth.info.email
+    user.image = auth.info.image
+    user.username = auth.info.username
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user.password = Devise.friendly_token[0,20]
+
   end
-
-
+  end
 end  
