8a95beeaadc2a4aaad272c3c7080cb7f78ab0c30
Fatema Mohamed <45967448+fatemaMO@users.noreply.github.com>
2019-03-29 18:05:18 +0200

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
     validates :restaurant_name, presence: true,
                     length: { minimum: 3 }
     validates :meal, presence: true
-    has_many :order_users, dependent: :delete_all
+    has_many :orders_users, dependent: :delete_all
   
     has_many :order_details
     belongs_to :user
